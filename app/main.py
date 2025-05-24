eed9de0bb83eb8bf81ab44252e84b42cde4ae393
gkbishnoi07 <gkishan1kyt@gmail.com>
2025-05-24 06:41:28 +0530

    update main.py and index.html

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
 from playwright_tests.test_site import test_site
 
 app = FastAPI()
+
+# Add CORS middleware
+origins = [
+    "https://automated-web-testing.netlify.app",
+    # Add other allowed origins if any, or use "*" to allow all (not recommended for production)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # or ["*"] to allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
